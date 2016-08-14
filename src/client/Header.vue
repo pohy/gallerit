06c2a4bd5e6a6ee8499582b6e496a924979cc2bf
David Pohan <0ronon0@gmail.com>
2016-08-14 16:56:03 +0200

    FE: Enable filters

--- src/client/Header.vue
+++ src/client/Header.vue
@@ -13,12 +13,25 @@
                         class="form-control"
                         v-model="subreddits"
                 />
-                <button class="btn btn-primary">Search</button>
-                <select class="form-control" v-model="sorting">
+                <select
+                        class="form-control"
+                        v-model="sorting"
+                        @change="submitForm"
+                >
                     <option v-for="sort in sortOptions" :value="sort.value">
                         {{ sort.title }}
                     </option>
                 </select>
+                <label class="form-check-inline">
+                    <input
+                            class="form-check-input"
+                            type="checkbox"
+                            v-model="nsfw"
+                            @change="submitForm"
+                    >
+                    NSFW
+                </label>
+                <button class="btn btn-primary">Search</button>
                 <button disabled class="btn btn-success">Start slideshow</button>
             </form>
         </div>
@@ -34,6 +47,7 @@
             return {
                 subreddits: 'pics',
                 sorting: 'hot',
+                nsfw: false,
                 sortOptions: [{
                     title: 'Hot',
                     value: 'hot'
@@ -56,7 +70,7 @@
         },
         methods: {
             submitForm() {
-                this.loadImages(this.subreddits)
+                this.loadImages(this.subreddits, this.sorting, this.nsfw)
             }
         }
     }
--- src/client/actions.js
+++ src/client/actions.js
@@ -1,9 +1,13 @@
 import * as types from './mutationTypes';
 
-export const loadImages = ({dispatch}, subreddits) => {
+export const loadImages = ({dispatch}, subreddits, sorting, nsfw) => {
     dispatch(types.LOAD_IMAGES_START);
     // TODO: remove the hardcoded URL
-    fetch(`http://localhost:3000?subreddits=${subreddits.replace(' ', '')}`)
+    fetch('http://localhost:3000'
+        + `?subreddits=${subreddits.replace(' ', '')}`
+        + `&sorting=${sorting}`
+        + `&nsfw=${nsfw}`
+    )
         .then((res) => res.json())
         .then((images) => dispatch(types.LOAD_IMAGES_SUCCESS, images))
         .catch(() => dispatch(types.LOAD_IMAGES_FAIL));
